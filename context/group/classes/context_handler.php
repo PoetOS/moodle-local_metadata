604f390bdb324ff9216be57b1c1008d8169d754b
Mike Churchward <mike.churchward@poetgroup.org>
2017-04-26 11:35:18 -0400

    POET - Moving stuff around.

---
+++ context/group/classes/context_handler.php
@@ -0,0 +1,138 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package local_metadata
+ * @author Mike Churchward <mike.churchward@poetgroup.org>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright 2016 POET
+ */
+
+/**
+ * Group metadata context handler class..
+ *
+ * @package local_metadata
+ * @copyright  2016 POET
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace metadatacontext_group;
+
+defined('MOODLE_INTERNAL') || die;
+
+class context_handler extends \local_metadata\context\context_handler {
+
+    /**
+     * Return the instance of the context. Must be handled by the implementing class.
+     * @return object The Moodle data record for the instance.
+     */
+    public function get_instance() {
+        global $DB;
+        if (empty($this->instance)) {
+            if (!empty($this->instanceid)) {
+                $this->instance = $DB->get_record('groups', ['id' => $this->instanceid], '*', MUST_EXIST);
+            } else {
+                $this->instance = false;
+            }
+        }
+        return $this->instance;
+    }
+
+    /**
+     * Return the instance of the context. Must be handled by the implementing class.
+     * @return object The Moodle context.
+     */
+    public function get_context() {
+        if (empty($this->context)) {
+            if (!empty($this->instance)) {
+                $this->context = \context_course::instance($this->instance->courseid);
+            } else {
+                $this->context = false;
+            }
+        }
+        return $this->context;
+    }
+
+    /**
+     * Return the instance of the context. Defaults to the home page.
+     * @return object The Moodle redirect URL.
+     */
+    public function get_redirect() {
+        return new \moodle_url('/group/group.php', ['id' => $this->instanceid, 'courseid' => $this->instance->courseid]);
+    }
+
+    /**
+     * Check any necessary access restrictions and error appropriately. Must be implemented.
+     * e.g. "require_login()". "require_capability()".
+     * @return boolean False if access should not be granted.
+     */
+    public function require_access() {
+        global $DB;
+        $course = $DB->get_record('course', ['id' => $this->instance->courseid], '*', MUST_EXIST);
+        require_login($course);
+        require_capability('moodle/course:managegroups', $this->context);
+        return true;
+    }
+
+    /**
+     * Implement if specific context settings can be added to a context settings page (e.g. user preferences).
+     */
+    public function add_settings_to_context_menu($navmenu) {
+        global $PAGE;
+
+        if (method_exists($navmenu, 'find') && $navmenu->find('groups', \settings_navigation::TYPE_SETTING)) {
+            // Add the settings page to the groups settings menu, if enabled.
+            $navmenu->add('groups', new \admin_externalpage('group_metadata', get_string('groupmetadata', 'local_metadata'),
+                new \moodle_url('/local/metadata/index.php', ['contextlevel' => CONTEXT_GROUP]), ['moodle/site:config']));
+        }
+        // Add the settings page to the course settings menu.
+        $navmenu->add('courses', new \admin_externalpage('group_metadata', get_string('groupmetadata', 'local_metadata'),
+            new \moodle_url('/local/metadata/index.php', ['contextlevel' => CONTEXT_GROUP]), ['moodle/site:config']));
+        return true;
+    }
+
+    /**
+     * Hook function that is called when settings blocks are being built.
+     */
+    public function extend_settings_navigation($settingsnav, $context) {
+        global $PAGE;
+        if ($PAGE->pagetype == 'group-group') {
+            // Context level is CONTEXT_COURSE.
+            if ((get_config('local_metadata', 'groupmetadataenabled') == 1) &&
+                has_capability('moodle/course:managegroups', $context)) {
+
+                if ($settingnode = $settingsnav->find('groups', \settings_navigation::TYPE_SETTING)) {
+                    $strmetadata = get_string('groupmetadata', 'local_metadata');
+                    $groupid = $PAGE->url->param('id');
+                    $url = new \moodle_url('/local/metadata/index.php',
+                        ['id' => $groupid, 'action' => 'groupdata', 'contextlevel' => CONTEXT_GROUP]);
+                    $metadatanode = \navigation_node::create(
+                        $strmetadata,
+                        $url,
+                        \navigation_node::NODETYPE_LEAF,
+                        'metadata',
+                        'metadata',
+                        new \pix_icon('i/settings', $strmetadata)
+                    );
+                    if ($PAGE->url->compare($url, URL_MATCH_BASE)) {
+                        $metadatanode->make_active();
+                    }
+                    $settingnode->add_node($metadatanode);
+                }
+            }
+        }
+    }
+}
---
+++ fieldtype/textarea/classes/metadata.php
@@ -0,0 +1,112 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Textarea profile field define.
+ *
+ * @package   profilefield_textarea
+ * @copyright  2007 onwards Shane Elliot {@link http://pukunui.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace metadatafieldtype_textarea;
+
+/**
+ * Class local_metadata_field_textarea.
+ *
+ * @copyright  2007 onwards Shane Elliot {@link http://pukunui.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class metadata extends \local_metadata\fieldtype\metadata {
+
+    /**
+     * Constructor method.
+     *
+     * @param int $fieldid
+     * @param int $instanceid
+     */
+    public function __construct($fieldid=0, $instanceid=0) {
+        // First call parent constructor.
+        parent::__construct($fieldid, $instanceid);
+
+        // Set the name for display; will need to be a language string.
+        $this->name = 'Text area';
+    }
+
+    /**
+     * Adds elements for this field type to the edit form.
+     * @param moodleform $mform
+     */
+    public function edit_field_add($mform) {
+        // Create the form field.
+        $mform->addElement('editor', $this->inputname, format_string($this->field->name), null, null);
+        $mform->setType($this->inputname, PARAM_RAW); // We MUST clean this before display!
+    }
+
+    /**
+     * Overwrite base class method, data in this field type is potentially too large to be included in the user object.
+     * @return bool
+     */
+    public function is_instance_object_data() {
+        return false;
+    }
+
+    /**
+     * Process incoming data for the field.
+     * @param stdClass $data
+     * @param stdClass $datarecord
+     * @return mixed|stdClass
+     */
+    public function edit_save_data_preprocess($data, $datarecord) {
+        if (is_array($data)) {
+            $datarecord->dataformat = $data['format'];
+            $data = $data['text'];
+        }
+        return $data;
+    }
+
+    /**
+     * Load instance data for this metadata field, ready for editing.
+     * @param stdClass $instance
+     */
+    public function edit_load_instance_data($instance) {
+        if ($this->data !== null) {
+            $this->data = clean_text($this->data, $this->dataformat);
+            $instance->{$this->inputname} = ['text' => $this->data, 'format' => $this->dataformat];
+        }
+    }
+
+    /**
+     * Display the data for this field
+     * @return string
+     */
+    public function display_data() {
+        return format_text($this->data, $this->dataformat, ['overflowdiv' => true]);
+    }
+
+    /**
+     * Return the field type and null properties.
+     * This will be used for validating the data submitted by a user.
+     *
+     * @return array the param type and null property
+     * @since Moodle 3.2
+     */
+    public function get_field_properties() {
+        return [PARAM_RAW, NULL_NOT_ALLOWED];
+    }
+}
+
+
